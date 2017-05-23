518bc64d7056b0f0dd3e5c04d0d3d1559642eaf3
Cheeto836 <mprussell836@gmail.com>
2017-05-23 19:48:00 -0400

    QcpVariable initial interface finished

--- Inc/QcpVariable.h
+++ Inc/QcpVariable.h
@@ -1,22 +1,52 @@
 #ifndef QCP_VARIABLE_H
 #define QCP_VARIABLE_H
 
+#include <QVariant>
+#include <QVector>
+
 #include "CommandOptions.h"
+
+enum class Type;
 
 class QcpVariable
 {
   private:
     //member variables
+    QVariant m_value; //value the variable holds
+    Type m_type; //type of the variable
 
   public:
+    //public enums
+    enum class Type
+    {
+      DOUBLE,
+      DOUBLE_ARRAY
+    };
+
     //constructors
     QcpVariable();
+    template <typename T> QcpVariable(Type type, T value)
+    {
+    } //end  template <typename T> QcpVariable(Type type, T value)
+    QcpVariable(const QcpVariable& toCopy);
+    ~QcpVariable();
 
     //public functions
+    QString fileText();
+    template <typename T> bool changeValue(Type newType, T newValue)
+    {
+    }
 
     //getters
+    Type getType();
+    template <typename T> getValue()
+    {
+    }
 
-    //setters
+    //op overloads
+    QcpVariable& operator=(const QcpVariable& toCopy);
+    QcpVariable& operator=(double value);
+    QcpVariable& operator=(const QVector<double>& value);
 };
 
 #endif //end #ifndef QCP_VARIABLE_H
--- src/QcpVariable.cpp
+++ src/QcpVariable.cpp
@@ -6,3 +6,36 @@
 QcpVariable::QcpVariable()
 {
 } //end QcpVariable::QcpVariable()
+QcpVariable::QcpVariable(const QcpVariable& toCopy)
+{
+}
+QcpVariable::~QcpVariable()
+{
+}
+
+//--------------------------------------------------
+//public functions
+//--------------------------------------------------
+QString QcpVariable::fileText()
+{
+}
+
+//--------------------------------------------------
+//getters
+//--------------------------------------------------
+Type getType()
+{
+}
+
+//--------------------------------------------------
+//op overloads
+//--------------------------------------------------
+QcpVariable& QcpVariable::operator=(const QcpVariable& toCopy)
+{
+}
+QcpVariable& QcpVariable::operator=(double value)
+{
+}
+QcpVariable& QcpVariable::operator=(const QVector<double>& value)
+{
+}
