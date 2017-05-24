344b68ea2c5e24f968797bdb14e89126e90b7aba
Cheeto836 <mprussell836@gmail.com>
2017-05-23 20:03:43 -0400

    finished initial interface for QcpGroup

--- Inc/QcpGroup.h
+++ Inc/QcpGroup.h
@@ -1,22 +1,30 @@
 #ifndef QCP_GROUP_H
 #define QCP_GROUP_H
 
+#include <QVector>
+#include <QString>
+
 #include "CommandOptions.h"
+#include "QcpVariable.h"
 
 class QcpGroup
 {
   private:
     //member variables
+    QVector<QcpVariable> m_vars;
 
   public:
     //constructors
     QcpGroup();
+    QcpGroup(const QVector<QcpVariable>& vars);
 
     //public functions
+    size_t size();
+    QString fileText();
 
-    //getters
-
-    //setters
+    //op overloads
+    QcpVariable& operator[](const int index);
+    const QcpVariable& operator[](const int index) const;
 }; //end class QcpGroup
 
 #endif //end #define QCP_GROUP_H
--- src/QcpGroup.cpp
+++ src/QcpGroup.cpp
@@ -6,3 +6,26 @@
 QcpGroup::QcpGroup()
 {
 } //end QcpGroup::QcpGroup()
+QcpGroup::QcpGroup(const QVector<QcpVariable>& vars)
+{
+}
+
+//--------------------------------------------------
+//public functions
+//--------------------------------------------------
+size_t QcpGroup::size()
+{
+}
+QString QcpGroup::fileText()
+{
+}
+
+//--------------------------------------------------
+//op overloads
+//--------------------------------------------------
+QcpVariable& QcpGroup::operator[](const int index)
+{
+}
+const QcpVariable& QcpGroup::operator[](const int index) const
+{
+}
