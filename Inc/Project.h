b4ee5b7e48c9bd98c97c5d56af5d9a0504347211
Cheeto836 <mprussell836@gmail.com>
2017-05-26 19:27:39 -0400

    finished Project class's initial interface

--- Inc/Project.h
+++ Inc/Project.h
@@ -1,22 +1,48 @@
 #ifndef QCP_PROJECT_H
 #define QCP_PROJECT_H
 
+#include <QString>
+
+#include <vector>
+#include <map>
+#include <utility>
+#include <fstream>
+
 #include "CommandOptions.h"
+#include "QcpFile.h"
 
 class Project
 {
   private:
     //member variables
+    std::map<QString, QcpFile> m_files; //first: name of file | second: file ADT
+    QString m_name; //name of the project
+
+    //private functions
+    void init(const QString& name, const std::map<QString, QcpFile>& files);
 
   public:
     //constructors
     Project();
+    Project(const QString& name);
+    Project(const QString& name, const std::map<QString, QcpFile>& files);
 
     //public functions
+    void addFile(const QString& name, const QcpFile& file);
+    QcpFile removeFile(const QString& name);
+    void clearFiles();
+    QcpFile& at(const QString& name);
+    const QcpFile& at(const QString& name) const;
 
     //getters
+    QString getName() const;
 
     //setters
+    void setName(const QString& name);
+
+    //op overloads
+    QcpFile& operator[](const QString& name);
+    const QcpFile& operator[](const QString& name) const;
 };
 
 #endif //end #ifndef QCP_PROJECT_H
--- src/Project.cpp
+++ src/Project.cpp
@@ -1,8 +1,64 @@
 #include "Project.h"
 
 //--------------------------------------------------
+//private functions
+//--------------------------------------------------
+void Project::init(const QString& name, const std::map<QString, QcpFile>& files)
+{
+}
+
+//--------------------------------------------------
 //constructors
 //--------------------------------------------------
 Project::Project()
 {
 } //end Project::Project()
+Project::Project(const QString& name)
+{
+}
+Project::Project(const QString& name, const std::map<QString, QcpFile>& files)
+{
+}
+
+//--------------------------------------------------
+//public functions
+//--------------------------------------------------
+void Project::addFile(const QString& name, const QcpFile& file)
+{
+}
+QcpFile Project::removeFile(const QString& name)
+{
+}
+void Project::clearFiles()
+{
+}
+QcpFile& Project::at(const QString& name)
+{
+}
+const QcpFile& Project::at(const QString& name) const
+{
+}
+
+//--------------------------------------------------
+//getters
+//--------------------------------------------------
+QString Project::getName() const
+{
+}
+
+//--------------------------------------------------
+//setters
+//--------------------------------------------------
+void Project::setName(const QString& name)
+{
+}
+
+//--------------------------------------------------
+//op overloads
+//--------------------------------------------------
+QcpFile& Project::operator[](const QString& name)
+{
+}
+const QcpFile& Project::operator[](const QString& name) const
+{
+}
