a89325a5a5b8192fd93f3739ea98ad417717051a
Cheeto836 <mprussell836@gmail.com>
2017-06-06 19:36:44 -0400

    finished initial version of Project class

--- Inc/Project.h
+++ Inc/Project.h
@@ -3,8 +3,9 @@
 
 #include <QString>
 #include <QVector>
+#include <QDir>
+#include <QFileInfo>
 
-#include <utility>
 #include <fstream>
 
 #include "CommandOptions.h"
@@ -16,31 +17,35 @@ class Project
     //member variables
     QVector<QcpFile> m_files; //list of files in the project
     QString m_name; //name of the project
+    QString m_location; //location where the project file gets saved
+    static int m_defaultNameNum; //used for default naming of projects
 
     //private functions
-    void init(const QString& name, const QMap<QString, QcpFile>& files);
+    void init(const QString& name, const QString& location, const QVector<QcpFile>& files);
+    int findFile(const QString& fileName) const;
 
   public:
     //constructors
     Project();
-    Project(const QString& name);
-    Project(const QString& name, const QMap<QString, QcpFile>& files);
+    Project(const QString& name, const QString& location);
+    Project(const QString& name, const QString& location, const QVector<QcpFile>& files);
 
     //public functions
     void addFile(const QcpFile& file);
     QcpFile removeFile(const QString& name);
     void clearFiles();
+    QcpFile& at(const QString& name);
 
     //getters
     QString getName() const;
-    QcpFile& getFile(const QString& name);
+    QString getLocation() const;
 
     //setters
     void setName(const QString& name);
+    void setLocation(const QString& location);
 
     //op overloads
     QcpFile& operator[](const QString& name);
-    const QcpFile& operator[](const QString& name) const;
 };
 
 #endif //end #ifndef QCP_PROJECT_H
--- src/Project.cpp
+++ src/Project.cpp
@@ -1,10 +1,87 @@
 #include "Project.h"
 
 //--------------------------------------------------
+//static member variables
+//--------------------------------------------------
+int Project::m_defaultNameNum = 1;
+
+//--------------------------------------------------
 //private functions
 //--------------------------------------------------
-void Project::init(const QString& name, const QMap<QString, QcpFile>& files)
+void Project::init(const QString& name, const QString& location,
+                   const QVector<QcpFile>& files)
 {
+  //handle blank names
+  QString realName;
+  if (std::all_of(name.toStdString().begin(), name.toStdString().end(), isspace))
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    {
+      std::cout << "Default name being used for Qcp Project because one wasn't provided"
+                << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    realName = "QcpProject" + QString::fromStdString(std::to_string(m_defaultNameNum));
+  } //end  if (std::all_of(name.toStdString().begin(), name.toStdString().end(), isspace))
+  else
+  {
+    realName = name;
+  } //end  else
+
+  //handle locations that don't exist
+  QString realLocation = QDir::homePath();
+  QFileInfo checkExists(location);
+  if (std::all_of(location.toStdString().begin(), location.toStdString().end(),
+                  isspace))
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+    {
+      std::cout << "ERROR: Blank locaiton provided to project " << m_name.toStdString()
+                << ". File will be saved in " << QDir::homePath().toStdString()
+                << " by default." << std::endl;
+    }
+  } //end  if (std::all_of(location.toStdString().begin(), location.toStdString().end(),
+  else
+  {
+    if (checkExists.exists() && checkExists.isDir())
+    {
+      realLocation = location;
+    } //end  if (checkExists.exists() && checkExists.isDir())
+    else
+    {
+      if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+      {
+        std::cout << "ERROR: Invalid file location " << location.toStdString()
+                  << "provided for project " << m_name.toStdString() << "." << std::endl
+                  << "Project will be saved in " << QDir::homePath().toStdString()
+                  << " instead." << std::endl;
+      }
+    } //end  else
+  } //end  else
+
+  //set all values
+  setName(realName);
+  setLocation(realLocation);
+  for (QcpFile file : files)
+  {
+    addFile(file);
+  } //end  for (QcpFile file : files)
+}
+int Project::findFile(const QString& name) const
+{
+  bool found = false;
+  int index = 0;
+  while (index < m_files.size() && !found)
+  {
+    if (m_files[index].getName() == name)
+    {
+      found = true;
+    } //end  if (m_vars[index].getName() == name)
+    else
+    {
+      index++;
+    } //end  else
+  } //end  while (index < m_vars.size() && !found)
+  return (found ? index : -1);
 }
 
 //--------------------------------------------------
@@ -12,31 +89,85 @@ void Project::init(const QString& name, const QMap<QString, QcpFile>& files)
 //--------------------------------------------------
 Project::Project()
 {
-} //end Project::Project()
-Project::Project(const QString& name)
-{
+  init("", "", {});
 }
-Project::Project(const QString& name, const QMap<QString, QcpFile>& files)
+Project::Project(const QString& name, const QString& location)
 {
+  init(name, location, {});
+}
+Project::Project(const QString& name, const QString& location,
+                 const QVector<QcpFile>& files)
+{
+  init(name, location, files);
 }
 
 //--------------------------------------------------
 //public functions
 //--------------------------------------------------
-void Project::addFile(const QString& name, const QcpFile& file)
+void Project::addFile(const QcpFile& file)
 {
+  int foundAt = findFile(file.getName());
+  if (foundAt != -1)
+  {
+    //file exists, can't add it again
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+    {
+      std::cout << "ERROR: cannot add file " << file.getName().toStdString()
+                << " to project " << m_name.toStdString() << " because it already exists"
+                << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+  } //end  if (foundAt != -1)
+  else
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    {
+      std::cout << "INFO: adding file " << file.getName().toStdString() << " to project "
+                << m_name.toStdString() << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    m_files.push_back(file);
+  } //end  else
 }
 QcpFile Project::removeFile(const QString& name)
 {
+  int foundAt = findFile(name);
+  if (foundAt != -1)
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    {
+      std::cout << "INFO: removing file " << m_files[foundAt].getName().toStdString()
+                << " from project " << m_name.toStdString() << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+    m_files.erase(m_files.begin() + foundAt);
+  } //end  if (foundAt != -1)
+  else
+  {
+    //file not found, inform user if debug level is high enough
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_AND_WARNINGS)
+    {
+      std::cout << "WARNING: << cannot remove file " << name.toStdString()
+                << "from project " << m_name.toStdString()
+                << " because it doesn't exist." << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_AND_WARNINGS)
+  } //end  else
 }
 void Project::clearFiles()
 {
+  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  {
+    std::cout << "INFO: Clearing all files from project " << m_name.toStdString()
+              << std::endl;
+  } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ALL_INFO)
+  m_files.clear();
 }
 QcpFile& Project::at(const QString& name)
 {
-}
-const QcpFile& Project::at(const QString& name) const
-{
+  int foundAt = findFile(name);
+  if (foundAt == -1)
+  {
+    throw std::out_of_range("File " + name.toStdString() + " doesn't exist in project " +
+                            m_name.toStdString());
+  } //end  if (foundAt == -1)
+  return m_files[foundAt];
 }
 
 //--------------------------------------------------
@@ -44,6 +175,11 @@ const QcpFile& Project::at(const QString& name) const
 //--------------------------------------------------
 QString Project::getName() const
 {
+  return m_name;
+}
+QString Project::getLocation() const
+{
+  return m_location;
 }
 
 //--------------------------------------------------
@@ -51,6 +187,37 @@ QString Project::getName() const
 //--------------------------------------------------
 void Project::setName(const QString& name)
 {
+  //handle blank names
+  if (std::all_of(name.toStdString().begin(), name.toStdString().end(), isspace))
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+    {
+      std::cout << "ERROR: Attempt to set project " << m_name.toStdString()
+                << " to an empty string. Name not being updated" << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+  } //end  if (std::all_of(name.toStdString().begin(), name.toStdString().end(), isspace))
+  else
+  {
+    m_name = name;
+  } //end  else
+}
+void Project::setLocation(const QString& location)
+{
+  //handle file locations that don't exist
+  QFileInfo fileExists(location);
+  if (fileExists.exists() && fileExists.isDir())
+  {
+    m_location = location;
+  } //end  if (fileExists.exists() && fileExists.isDir())
+  else
+  {
+    if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+    {
+      std::cout << "ERROR: Attempt to set project " << m_name.toStdString()
+                << "'s locaiton to" << location.toStdString()
+                << " failed because that location doesn't exist" << std::endl;
+    } //end  if (CmdOptions::verbosity >= CmdOptions::DEBUG_LEVEL::ERRORS_ONLY)
+  } //end  else
 }
 
 //--------------------------------------------------
@@ -58,7 +225,5 @@ void Project::setName(const QString& name)
 //--------------------------------------------------
 QcpFile& Project::operator[](const QString& name)
 {
-}
-const QcpFile& Project::operator[](const QString& name) const
-{
+  return at(name);
 }
